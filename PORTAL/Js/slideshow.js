75d6855fb7069158a8de736acc7d66f1a234a231
Badfox16 <123194105+Badfox16@users.noreply.github.com>
2023-02-26 13:46:16 +0200

    Add files via upload

---
+++ PORTAL/Js/slideshow.js
@@ -0,0 +1,25 @@
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+  
+  // Apply to all slideshows that you define with the markup wrote
+  slideshows.forEach(initSlideShow);
+
+  function initSlideShow(slideshow) {
+
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Buscar um array(lista) de imgs para o slide 
+
+    var indice = 0, time = 5000;
+    slides[indice].classList.add('active');  
+    
+    setInterval( () => {
+      slides[indice].classList.remove('active');
+      
+      //Atravessar todo o slide, atraves do indice de cada imagem
+      indice++;
+      
+      //Se percorrer todas as imagens do slideshow, o indice volta ser zero de modo a mostrar o primeiro slide outra vez
+      if (indice === slides.length) indice = 0; 
+      
+      slides[indice].classList.add('active');
+
+    }, time);
+  }
