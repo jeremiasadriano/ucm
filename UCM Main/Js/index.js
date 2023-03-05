f03084ebbb9ae27b430f36eb98c6f3d1374a8233
Pedro Nhamirre <pedrooliv62@gmail.com>
2023-03-05 14:07:47 +0200

    going stong

--- UCM Main/Js/index.js
+++ UCM Main/Js/index.js
@@ -8,10 +8,44 @@ document.getElementById("button2").onclick = function () {
 
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.width = "80%";
+  document.getElementById("myNav").style.width = "90%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
-} 
+}
+
+//______CAROUSEL___________
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
