a2f1f1aac0aab452cb1dc1779ecaa0b29c88e5cc
Pedro Nhamirre <pedrooliv62@gmail.com>
2023-03-05 09:26:46 +0200

    ainda trabalhando

--- UCM Main/Js/index.js
+++ UCM Main/Js/index.js
@@ -6,51 +6,12 @@ document.getElementById("button2").onclick = function () {
   location.href = "https://esura.ucm.ac.mz/inscricoes/registrationhelp";
 };
 
-//SLIDEHOW
-let slideIndex = 0;
-showSlidesAuto();
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "80%";
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-
-function showSlidesAuto() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlidesAuto, 10000); // Change image every 2 seconds
-}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+} 
